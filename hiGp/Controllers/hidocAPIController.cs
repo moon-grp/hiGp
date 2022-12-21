b61dcd3febad0c93557b33740cddc50e2d542574
emohex <olumidemm@gmail.com>
2022-12-21 13:36:45 +0100

    Add project files.

---
+++ hiGp/Controllers/hidocAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hiGp.Controllers
+{
+    [Route("api/v1/hidoc")]
+    [ApiController]
+    public class hidocAPIController: ControllerBase 
+    {
+    }
+}
